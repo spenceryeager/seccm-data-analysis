7f565c1cb7c26627c7151b1ea89c2d5db4a92a67
Spencer Yeager <yeager.spencer@gmail.com>
2023-01-18 00:46:21 -0700

    Added formatting changes and new energy scale

--- figure-plotting/cv-overlay.py
+++ figure-plotting/cv-overlay.py
@@ -10,6 +10,10 @@ def main():
 
 
 def make_plot():
+    # Setting font size before making plot
+    font = {'size': 12}
+    plt.rc('font', **font)
+
     directory = r'dir'
     file_list = os.listdir(directory)
     v = 'Voltage (V)'
@@ -18,6 +22,7 @@ def make_plot():
     # some parameters to change
     sweep_numbers = 3
     fc_calibration = 0.5
+    fc_ev = -4.9
 
     # creating plot
     fig, ax = plt.subplots()
@@ -27,15 +32,18 @@ def make_plot():
             cv_count += 1
             data = pd.read_csv(os.path.join(directory, file), sep='\t') # loading in data
             second_sweep = int(len(data) / sweep_numbers) # getting length of data file to remove first sweep
-            
-            ax.plot(data[v][second_sweep:] - fc_calibration, data[i][second_sweep:] * -1, color='red', alpha=0.1)
+            ax.plot(data[v][second_sweep:] - fc_calibration, data[i][second_sweep:] * -1, color='red', alpha=0.05)
 
     # Formatting plot
     ax.invert_xaxis()
     ax.minorticks_on()
     ax.set_xlabel("Potential vs. Fc/Fc$^{+}$")
     ax.set_ylabel("Current pA)")
-    ax.legend(['n ='+str(cv_count)], handlelength=0, handletextpad=0)    
+    ax.legend(['n ='+str(cv_count)], handlelength=0, handletextpad=0)
+
+    ax2 = ax.secondary_xaxis("top", functions=(lambda x: (x-fc_ev)*-1, lambda x: (x+fc_ev)*-1))
+    ax2.minorticks_on()
+    ax2.set_xlabel("Energy vs. Vacuum (eV)")    
     plt.show()
 
 
