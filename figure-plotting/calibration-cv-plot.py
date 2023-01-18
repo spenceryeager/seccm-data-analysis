e405a88f3158f85433df71fe20ee4696025e0457
Spencer Yeager <yeager.spencer@gmail.com>
2023-01-17 22:39:50 -0700

    Added secondary axis and formatting

--- figure-plotting/calibration-cv-plot.py
+++ figure-plotting/calibration-cv-plot.py
@@ -7,28 +7,40 @@ def main():
 
 
 def display_plot():
+    fc_redox_correction = 0.5
+
     # Data loading
-    initial_scan_loc = 'enter filepath'
-    initial_data = pd.read_csv(data_location)
-    final_scan_loc = 'enter filepath'
-    final_data = pd.read_csv(final_scan_loc)
+    initial_scan_loc = r"dir"
+    initial_data = pd.read_csv(initial_scan_loc,
+                                sep = '\t')
+    final_scan_loc = r"dir"
+    final_data = pd.read_csv(final_scan_loc,
+                                sep = '\t')
     v = 'Voltage (V)' # file headers
     i = 'Current (pA)'
 
     # Plotting
     fig, ax = plt.subplots()
-    ax.plot(initial_data[v], initial_data[i], 
+    ax.plot(initial_data[v], initial_data[i] * -1, 
             color='red', 
             label='Initial Scan')
 
-    ax.plot(final_data[v], final_data[i],
-            color='blue',
+    ax.plot(final_data[v], final_data[i] * -1,
+            color='purple',
             label='Final Scan')
     
-    ax.set_xlabel('Potential vs. Ag Wire')
+    # Secondary axis
+    ax2 = ax.secondary_xaxis("top", functions=(lambda x: x-fc_redox_correction, lambda x: x+fc_redox_correction))
+    ax2.minorticks_on()
+
+    # Formatting plot
+    ax.set_xlabel('Potential vs. Ag Wire (V)')
+    ax2.set_xlabel('Potential vs. Fc/Fc$^{+}$ (V)')
     ax.set_ylabel('Current (pA)')
+    ax.invert_xaxis()
+    ax.minorticks_on()
     ax.legend()
-    plt.show
+    plt.show()
 
 
 if __name__ == '__main__':
