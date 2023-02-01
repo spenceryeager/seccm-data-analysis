7866a455e95b825b52665b37fafe18b9079afe3e
Spencer Yeager <yeager.spencer@gmail.com>
2023-01-31 18:17:38 -0700

    shell program created

---
+++ movie-making/current_voltage_movie.py
@@ -0,0 +1,16 @@
+# This program will plot the current for each voltage at its corresponding XY value
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def main():
+    make_plot()
+
+
+def make_plot():
+    print('Hello')
+
+
+if __name__ == "__main__":
+    main()
