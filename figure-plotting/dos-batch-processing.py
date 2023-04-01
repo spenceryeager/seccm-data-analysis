b6ad320ce16cba3f80b1e57504c28b60a4b002e1
Spencer Yeager <yeager.spencer@gmail.com>
2023-04-01 15:57:53 -0700

    had to add os import and updated to do a full path joing

--- figure-plotting/dos-batch-processing.py
+++ figure-plotting/dos-batch-processing.py
@@ -3,15 +3,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import scipy.signal as signal
-import scipy.ndimage as ndimage
-import scipy.constants as constant
-import scipy.stats as stats
+import os
 from dos_calculation import dos_array_return
+from directory_sorting import directory_sort
 
 
 def main():
-    print('hello')
+    data_directory = r"C:\Users\spenceryeager\Documents\seccm-data\19Mar2023_rrP3HT_500nm-tip\scan1"
+    file_list = []
+    sorted_file_list = directory_sort(data_directory)
+    print(sorted_file_list)
+
 
 
 if __name__ == "__main__":
