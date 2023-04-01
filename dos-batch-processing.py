4ad96f961e7f5171b7c45297c50e2f079fc2afc6
Spencer Yeager <yeager.spencer@gmail.com>
2023-04-01 14:22:00 -0700

    initialized file

---
+++ dos-batch-processing.py
@@ -0,0 +1,9 @@
+# batch file for calculating DOS from all SECCM runs.
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import scipy.signal as signal
+import scipy.ndimage as ndimage
+import scipy.constants as constant
+import scipy.stats as stats
