b8ad3b239f257105a8d6b7f352f1192e503abda1
Spencer Yeager <yeager.spencer@gmail.com>
2023-04-01 15:10:45 -0700

    I am a dumbass and used dashes in my filename. Throws syntax error if trying to import :(

--- figure-plotting/dos-batch-processing.py
+++ figure-plotting/dos-batch-processing.py
@@ -7,3 +7,12 @@ import scipy.signal as signal
 import scipy.ndimage as ndimage
 import scipy.constants as constant
 import scipy.stats as stats
+from dos_calculation import dos_array_return
+
+
+def main():
+    print('hello')
+
+
+if __name__ == "__main__":
+    main()
