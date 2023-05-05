7677d5df6f356d2867987923373f4230129aa771
Spencer Yeager <yeager.spencer@gmail.com>
2023-05-05 13:34:21 -0700

    fixed background current issue

--- data-processing/kinetics.py
+++ data-processing/kinetics.py
@@ -9,7 +9,7 @@ import scipy.constants as constant
 import scipy.stats as stats
 
 def main():
-    data_path = r"D:\Research\SPECS-Project\2023\18Feb2023_rrP3HT_500nm-tip-Fc\scan\0X_2Y_rrP3HT-Fc.csv"
+    data_path = r"C:\Users\spenceryeager\Documents\seccm-data\18Feb2023_rrP3HT_500nm-tip-Fc\scan\0X_2Y_rrP3HT-Fc.csv"
     data = pd.read_csv(data_path, sep='\t')
     length = len(data)
     ox_start = int(length/3)
@@ -17,15 +17,16 @@ def main():
     ox_start = ox_start + 10
     lin_start = 0.15
     lin_end = 0.25
-    cleaned_current = current_cleanup(data['Current (pA)'][ox_start:ox_end])
-    print(data[ox_start:ox_end])
-    # corrected_current = background_correction(data['Voltage (V)'][ox_start:ox_end], cleaned_current, lin_start, lin_end)
+    data = data[ox_start:ox_end]
+    cleaned_current = current_cleanup(data['Current (pA)'])
+    # print(data)
+    corrected_current = background_correction(data, cleaned_current, lin_start, lin_end)
 
-    # fig, ax = plt.subplots()
-    # ax.plot(data['Voltage (V)'][ox_start:ox_end], cleaned_current, color='red')
-    # ax.plot(data['Voltage (V)'][ox_start:ox_end], corrected_current, color='blue')
+    fig, ax = plt.subplots()
+    ax.plot(data['Voltage (V)'], cleaned_current, color='red', label='Uncorrected')
+    ax.plot(data['Voltage (V)'], corrected_current, color='blue', label='Background corrected')
 
-    # plt.show()
+    plt.show()
 
 
 def current_cleanup(current_data):
@@ -35,11 +36,11 @@ def current_cleanup(current_data):
 
 
 def background_correction(data_subset, cleaned_current, lin_start, lin_end):
-    # data_subset = data_subset.reset_index(drop=True)
-    print(data_subset)
+    data_subset = data_subset.reset_index(drop=True)
     lin_start_index = data_subset.loc[data_subset['Voltage (V)'] == lin_start].index[0]
     lin_end_index = data_subset.loc[data_subset['Voltage (V)'] == lin_end].index[0]
-    lin_regression = stats.linregress(data_subset['Voltage (V)'][lin_start_index:lin_end_index], cleaned_current[lin_start_index:lin_end_index])
+    # print(cleaned_current[lin_end_index:lin_start_index])
+    lin_regression = stats.linregress(data_subset['Voltage (V)'][lin_end_index:lin_start_index], cleaned_current[lin_end_index:lin_start_index])
     background = (lin_regression[0] * data_subset['Voltage (V)']) + lin_regression[1]
     correction_vals = np.zeros(len(background))
     index = 0
@@ -49,7 +50,6 @@ def background_correction(data_subset, cleaned_current, lin_start, lin_end):
     # print(correction_vals)
     corrected_current = cleaned_current + correction_vals
     return(corrected_current)
-
 
 if __name__ == "__main__":
     main()
