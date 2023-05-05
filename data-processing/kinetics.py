7650dbcd84306c177e96032e06116332bf677962
Spencer Yeager <yeager.spencer@gmail.com>
2023-05-04 18:21:02 -0700

    added background correction and current cleanup

--- data-processing/kinetics.py
+++ data-processing/kinetics.py
@@ -3,6 +3,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy.signal as signal
+import scipy.ndimage as ndimage
+import scipy.constants as constant
+import scipy.stats as stats
 
 def main():
     data_path = r"D:\Research\SPECS-Project\2023\18Feb2023_rrP3HT_500nm-tip-Fc\scan\0X_2Y_rrP3HT-Fc.csv"
@@ -10,10 +14,36 @@ def main():
     length = len(data)
     ox_start = int(length/3)
     ox_end = ox_start + int(ox_start/2)
+    ox_start = ox_start + 10
+    cleaned_current = current_cleanup(data['Current (pA)'][ox_start:ox_end])
     
     fig, ax = plt.subplots()
-    ax.plot(data['Voltage (V)'][ox_start:ox_end], data['Current (pA)'][ox_start:ox_end])
+    ax.plot(data['Voltage (V)'][ox_start:ox_end], cleaned_current, color='red')
+    # ax.plot(data['Voltage (V)'][ox_start:ox_end], data['Current (pA)'][ox_start:ox_end], alpha=0.5, color='red')
+
     plt.show()
+
+
+def current_cleanup(current_data):
+    sav_gol = signal.savgol_filter(current_data, 10, 3)
+    cleaned_current = ndimage.gaussian_filter1d(sav_gol, 15)
+    return cleaned_current
+
+
+def background_correction(data_subset, cleaned_current, lin_start, lin_end):
+    # data_subset = data_subset.reset_index(drop=True)
+    lin_start_index = data_subset.loc[data_subset['Voltage (V)'] == lin_start].index[0]
+    lin_end_index = data_subset.loc[data_subset['Voltage (V)'] == lin_end].index[0]
+    lin_regression = stats.linregress(data_subset['Voltage (V)'][lin_start_index:lin_end_index], cleaned_current[lin_start_index:lin_end_index])
+    background = (lin_regression[0] * data_subset['Voltage (V)']) + lin_regression[1]
+    correction_vals = np.zeros(len(background))
+    index = 0
+    for val in background:
+        correction_vals[index] = (0 - val)
+        index += 1
+    # print(correction_vals)
+    corrected_current = cleaned_current + correction_vals
+    return(corrected_current)
 
 
 if __name__ == "__main__":
