66b9446f67c7af973b4d9d465531bf17d9eb8fb5
Spencer Yeager <yeager.spencer@gmail.com>
2023-05-19 18:43:01 -0700

    readjusting how this works

--- movie-making/current_voltage_movie.py
+++ movie-making/current_voltage_movie.py
@@ -10,7 +10,7 @@ def main():
 
 
 def make_plot():
-    dir_path = r"R:\Spencer Yeager\data\NiOx_Project\2023\01_Jan\30Jan2023_Juan-NiOx-400nm-tip\scan"
+    dir_path = r"C:\Users\spenceryeager\Documents\seccm-data\13May2023_JuanNiOx_500nmTip\scan"
     data_list = file_sort(dir_path)
     x_list = []
     y_list = []
@@ -24,38 +24,39 @@ def make_plot():
     X, Y = np.meshgrid(unique_x, unique_y)
 
     data_path = os.path.join(dir_path, data_list[1])
-    data = pd.read_csv(data_path, sep='\t')
-    index = len(data)
+    # data = pd.read_csv(data_path, sep='\t')
+    print(data_list)
+    index = len(data_list)
     file_count = 0
-    while file_count <= index:
-        current_list = []
-        fileskip = 0
-        for i in data_list:
-            if fileskip == 0:
-                current_list.append(0)
-                fileskip += 1
-            else:
-                data_path = os.path.join(dir_path, i)
-                data = pd.read_csv(data_path, sep='\t')
-                current_list.append(data['Current (pA)'][file_count])
-                voltage = str(data['Voltage (V)'][file_count])
+    # while file_count <= index:
+    #     current_list = []
+    #     fileskip = 0
+    #     for i in data_list:
+    #         if fileskip == 0:
+    #             current_list.append(0)
+    #             fileskip += 1
+    #         else:
+    #             data_path = os.path.join(dir_path, i)
+    #             data = pd.read_csv(data_path, sep='\t')
+    #             current_list.append(data['Current (pA)'][file_count])
+    #             voltage = str(data['Voltage (V)'][file_count])
 
-        # print(len(current_list))
+    #     # print(len(current_list))
 
-        xy_current = pd.DataFrame(list(zip(x_list, y_list, current_list)), columns = ['X', 'Y', 'Current (pA)'])
-        Z_current = xy_current.pivot_table(index="X", columns="Y", values="Current (pA)").T.values
+    #     xy_current = pd.DataFrame(list(zip(x_list, y_list, current_list)), columns = ['X', 'Y', 'Current (pA)'])
+    #     Z_current = xy_current.pivot_table(index="X", columns="Y", values="Current (pA)").T.values
 
-        fig, ax = plt.subplots()
-        im = ax.pcolormesh(X, Y, Z_current, vmin=-120, vmax=100)
-        ax.set_xlabel('X ($\\rm\mu$m)')
-        ax.set_ylabel('Y ($\\rm\mu$m)')
-        ax.set_title(voltage)
-        cb = fig.colorbar(im, ax=ax)
-        cb.set_label('Current (pA)')
-        savepath = r"C:\Users\spenceryeager\Documents\group_meetings\8Feb2023\400nm-tip\video"
-        savename = str(file_count) + ".png"
-        plt.savefig(os.path.join(savepath, savename))
-        file_count += 100
+    #     fig, ax = plt.subplots()
+    #     im = ax.pcolormesh(X, Y, Z_current, vmin=-120, vmax=100)
+    #     ax.set_xlabel('X ($\\rm\mu$m)')
+    #     ax.set_ylabel('Y ($\\rm\mu$m)')
+    #     ax.set_title(voltage)
+    #     cb = fig.colorbar(im, ax=ax)
+    #     cb.set_label('Current (pA)')
+    #     savepath = r"C:\Users\spenceryeager\Documents\group_meetings\8Feb2023\400nm-tip\video"
+    #     savename = str(file_count) + ".png"
+    #     plt.savefig(os.path.join(savepath, savename))
+    #     file_count += 100
 
 
 
