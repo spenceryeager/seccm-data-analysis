6d6f45f28a81b8c7a2cf934afaa5825cfad105a0
Spencer Yeager <yeager.spencer@gmail.com>
2023-06-06 18:19:45 -0700

    got the plot. just need to flip it somehow?

--- data-processing/oldham_zoski_sim.py
+++ data-processing/oldham_zoski_sim.py
@@ -14,24 +14,48 @@ def main():
     eo = 0.4
     e = np.linspace(-0.2, 0.8, 50)
     print(e)
-    ko = 1
-    transfer_coef = 0.5
-    theta(do, dr, n, eo, e)
-    # kappa(ko, transfer_coef, n, e, eo)
+    ko = 0.003
+    transfer_coef = 0.89
+    theta = theta_calc(do, dr, n, eo, e)
+    kappa = kappa_calc(ko, transfer_coef, n, e, eo)
+    val = current_potential_relation(theta, kappa)
+    print(val)
+    fig, ax = plt.subplots()
+    ax.plot(e, val)
+    plt.show()
+
     
 
-def theta(do, dr, n, eo, e):
+def theta_calc(do, dr, n, eo, e):
     temp = 298
     F = constant.physical_constants['Faraday constant'][0]
     exponential = np.exp((n * F * (e - eo)) / (temp * constant.R))
-    print(exponential)
+    theta_val = 1 + (do/dr) * exponential
+    return(theta_val)
 
 
-def kappa(ko, transfer_coef, n, e, eo):
+def kappa_calc(ko, transfer_coef, n, e, eo):
     temp = 298
     F = constant.physical_constants['Faraday constant'][0]
     kappa_val = ko * np.exp(((- n) * transfer_coef * F * (e - eo)) / (temp * constant.R))
-    print(kappa_val)
+    return(kappa_val)
+
+
+def current_potential_relation(theta, kappa):
+    # this is equation 13 in top paper. Going to break up most of it to make it easier to follow...
+    kappatheta = theta * kappa
+    numerator1 = (2 * kappatheta) + (3 * constant.pi)
+    denominator1 = (4 * kappatheta) + (3 * constant.pi ** 2)
+    fraction1 = numerator1 / denominator1
+    
+    numerator2 = constant.pi
+    denominator2 = kappatheta
+    fraction2 = numerator2 / denominator2
+    product1 = fraction2 * fraction1
+    inverse = np.reciprocal((1 + product1))
+    final_product = np.reciprocal(theta) * inverse
+    return final_product
+    
 
 
 if __name__ == "__main__":
