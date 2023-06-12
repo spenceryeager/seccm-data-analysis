08a9fe8f6546db11ff498d0cb2e11e26e66340b1
Spencer Yeager <yeager.spencer@gmail.com>
2023-06-12 16:55:55 -0700

    got a distribution!

--- data-processing/kinetics_histogram.py
+++ data-processing/kinetics_histogram.py
@@ -6,7 +6,22 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    print("hello")
+    kinetics_file = r"C:\Users\spenceryeager\Documents\seccm-data\analysis_folder\results.csv"
+    kinetics_data = pd.read_csv(kinetics_file)
+    observations = kinetics_data['log10 Rate Constant'].count()
+    bin_num = sturges_rule(observations)
+    plotting(kinetics_data, bin_num)
+
+
+def sturges_rule(observations):
+    bin_num = int(np.ceil(np.log2(observations) + 1))
+    return bin_num
+
+
+def plotting(data, bin_num):
+    fig, ax = plt.subplots()
+    ax.hist(data['log10 Rate Constant'], bin_num, edgecolor='royalblue', color='dodgerblue')
+    plt.show()
 
 
 if __name__ == "__main__":
