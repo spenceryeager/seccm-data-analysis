217f5564669168e87b41684810692f9c74e2b456
Spencer Yeager <yeager.spencer@gmail.com>
2023-06-12 15:32:45 -0700

    histogram stub

---
+++ data-processing/kinetics_histogram.py
@@ -0,0 +1,13 @@
+# this takes the output file from batch_kinetics_processing.py and does statistical analysis on it
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
