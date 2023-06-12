c98ccc8ee9393b1add261c04610c200023545a7c
Spencer Yeager <yeager.spencer@gmail.com>
2023-06-11 21:04:48 -0700

    working on the batch script

--- data-processing/batch_kinetics_processing.py
+++ data-processing/batch_kinetics_processing.py
@@ -5,11 +5,22 @@
 import pandas as pd
 import numpy as np
 from bard_mirkin_kinetics import get_kinetics
+import os
 
 
 def main():
-    print('hello')
-
+    directory = r"C:\Users\spenceryeager\Documents\seccm-data\test_dir"
+    filelist = os.listdir(directory)
+    print(filelist)
+    linear_region = [0.15, 0.29] # Defining start and end of linear region for background correction
+    formal_potential = 0.4 # V, formal redox potential of probe
+    id_potential = 0.2 # V, potential where diffusion-limited current is observed
+    diffusion_coef = 1 * (10 ** -6) # cm2/s
+    tip_radius = 2.5 * (10 **-5) #cm
+    for filename in filelist:
+        data_path = os.path.join(directory,filename)
+        rate_constant, kappa_naught, kappa_naught_error, transfer_coef, transfer_coef_error = get_kinetics(data_path, linear_region, diffusion_current_potential=id_potential, diffusion_coefficient=diffusion_coef, tip_radius=tip_radius, plotting=False)
+        print(np.log10(rate_constant))
 
 if __name__ == "__main__":
     main()
