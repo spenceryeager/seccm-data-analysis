309dc4d0b105979bea183d337ea13796412a01a7
Spencer Yeager <yeager.spencer@gmail.com>
2023-06-12 23:06:46 -0700

    updated parameters

--- data-processing/batch_kinetics_processing.py
+++ data-processing/batch_kinetics_processing.py
@@ -9,19 +9,21 @@ import os
 
 
 def main():
-    directory = r"C:\Users\spenceryeager\Documents\seccm-data\18Feb2023_rrP3HT_500nm-tip-Fc\scan"
-    save_dir = r"C:\Users\spenceryeager\Documents\seccm-data\analysis_folder"
-    save_name = 'results.csv'
+    directory = r"D:\Research\SPECS-Project\2023\24Mar2023_P3HT-with-Fc\scan"
+    save_dir = r"D:\Research\SPECS-Project\2023\24Mar2023_P3HT-with-Fc\analysis_folder"
+    save_name = 'results_with_bounds.csv'
+    settings_name = "settings.txt"
     filelist = os.listdir(directory)
     # print(filelist)
-    linear_region = [0.15, 0.29] # Defining start and end of linear region for background correction
+    linear_region = [0.24, 0.25] # Defining start and end of linear region for background correction
     formal_potential = 0.4 # V, formal redox potential of probe
-    id_potential = 0.2 # V, potential where diffusion-limited current is observed
+    id_potential = 0.24 # V, potential where diffusion-limited current is observed
     diffusion_coef = 1 * (10 ** -6) # cm2/s
-    tip_radius = 2.5 * (10 **-5) #cm
-    potential_range = [0.7, 0.10] # V!
-    sweep_number = 3
-    kinetics_df = pd.DataFrame(columns = ['Rate Constant (cm/s)', 'log10 Rate Constant', "Transfer Coefficient", "Transfer Coefficient Error", "KappaNaught", "KappaNaught Error"])
+    tip_radius = 2 * (10 **-5) #cm
+    potential_range = [0.6, 0.2] # V!
+    sweep_number = 2
+    kinetics_df = pd.DataFrame(columns = ['Half Potential (V)', 'Rate Constant (cm/s)', 'log10 Rate Constant', "Transfer Coefficient", "Transfer Coefficient Error", "KappaNaught", "KappaNaught Error"])
+    half_potential_list = []
     rate_constant_list = []
     transfer_coef_list = []
     transfer_coef_error_list = []
@@ -33,7 +35,8 @@ def main():
         if os.path.isfile(os.path.join(directory,filename)):
 
             data_path = os.path.join(directory,filename)
-            rate_constant, kappa_naught, kappa_naught_error, transfer_coef, transfer_coef_error = get_kinetics(data_path, linear_region, potential_range=potential_range, sweep=sweep_number, diffusion_current_potential=id_potential, diffusion_coefficient=diffusion_coef, tip_radius=tip_radius, plotting=False)
+            rate_constant, kappa_naught, kappa_naught_error, transfer_coef, transfer_coef_error, ehalf = get_kinetics(data_path, linear_region, potential_range=potential_range, sweep=sweep_number, diffusion_current_potential=id_potential, diffusion_coefficient=diffusion_coef, tip_radius=tip_radius, plotting=False)
+            half_potential_list.append(ehalf)
             rate_constant_list.append(rate_constant)
             transfer_coef_list.append(transfer_coef)
             transfer_coef_error_list.append(transfer_coef_error)
@@ -43,7 +46,7 @@ def main():
             print('skip')
 
 
-
+    kinetics_df['Half Potential (V)'] = half_potential_list
     kinetics_df['Rate Constant (cm/s)'] = rate_constant_list
     kinetics_df['log10 Rate Constant'] = np.log10(rate_constant_list)
     kinetics_df['Transfer Coefficient'] = transfer_coef_list
