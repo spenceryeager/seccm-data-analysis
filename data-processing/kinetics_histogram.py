2797b375c3481c11037f9b46043ed9ca3789ece8
Spencer Yeager <yeager.spencer@gmail.com>
2023-06-12 23:06:19 -0700

    added two histograms

--- data-processing/kinetics_histogram.py
+++ data-processing/kinetics_histogram.py
@@ -6,11 +6,13 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    kinetics_file = r"C:\Users\spenceryeager\Documents\seccm-data\analysis_folder\results.csv"
+    kinetics_file = r"D:\Research\SPECS-Project\2023\24Mar2023_P3HT-with-Fc\analysis_folder\results_with_bounds.csv"
+    kinetics_file_2 = r"D:\Research\SPECS-Project\2023\28Mar2023_PBTTT_Fc\analysis\results_with_bounds.csv"
     kinetics_data = pd.read_csv(kinetics_file)
+    kinetics_data_2 = pd.read_csv(kinetics_file_2)
     observations = kinetics_data['log10 Rate Constant'].count()
     bin_num = sturges_rule(observations)
-    plotting(kinetics_data, bin_num)
+    plotting(kinetics_data, kinetics_data_2, bin_num)
 
 
 def sturges_rule(observations):
@@ -18,9 +20,10 @@ def sturges_rule(observations):
     return bin_num
 
 
-def plotting(data, bin_num):
+def plotting(data, data2, bin_num):
     fig, ax = plt.subplots()
     ax.hist(data['log10 Rate Constant'], bin_num, edgecolor='royalblue', color='dodgerblue')
+    ax.hist(data2['log10 Rate Constant'], bin_num, edgecolor='firebrick', color='red')
     plt.show()
 
 
