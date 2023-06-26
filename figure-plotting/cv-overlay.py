094117dc84e92baa7ffc8e5f52638817f79d7009
Spencer Yeager <yeager.spencer@gmail.com>
2023-06-26 15:46:29 -0400

    updated cv overlay to be more consistent with current measurements

--- figure-plotting/cv-overlay.py
+++ figure-plotting/cv-overlay.py
@@ -14,39 +14,40 @@ def make_plot():
     font = {'size': 12}
     plt.rc('font', **font)
 
-    directory = r'dir'
+    directory = r"C:\Users\Spencer\Documents\data-analysis\28Mar2023_PBTTT_Fc\scan"
     file_list = os.listdir(directory)
     v = 'Voltage (V)'
     i = 'Current (pA)'
     
     # some parameters to change
     sweep_numbers = 3
-    fc_calibration = 0.5
+    fc_calibration = 0
     fc_ev = -4.9
 
     # creating plot
-    fig, ax = plt.subplots(dpi=300)
+    fig, ax = plt.subplots()
     cv_count = 0
     for file in file_list:
         if file.endswith('.csv'):
             cv_count += 1
             data = pd.read_csv(os.path.join(directory, file), sep='\t') # loading in data
             second_sweep = int(len(data) / sweep_numbers) # getting length of data file to remove first sweep
-            ax.plot(data[v][second_sweep:] - fc_calibration, data[i][second_sweep:] * -1, color='red', alpha=0.05)
+            ax.plot(data[v][second_sweep:] - fc_calibration, data[i][second_sweep:] * -1, color='dodgerblue', alpha=0.05)
 
     # Formatting plot
     ax.invert_xaxis()
     ax.minorticks_on()
-    ax.set_xlabel("Potential vs. Fc/Fc$^{+}$")
-    ax.set_ylabel("Current pA)")
+    ax.set_xlabel("Potential (V) vs. Ag/AgCl")
+    ax.set_ylabel("Current (pA)")
+    ax.set_ylim(-25,3)
     ax.legend(['n ='+str(cv_count)], handlelength=0, handletextpad=0)
 
-    ax2 = ax.secondary_xaxis("top", functions=(lambda x: (x-fc_ev)*-1, lambda x: (x+fc_ev)*-1))
-    ax2.minorticks_on()
-    ax2.set_xlabel("Energy vs. Vacuum (eV)")
+    # ax2 = ax.secondary_xaxis("top", functions=(lambda x: (x-fc_ev)*-1, lambda x: (x+fc_ev)*-1))
+    # ax2.minorticks_on()
+    # ax2.set_xlabel("Energy vs. Vacuum (eV)")
     plt.tight_layout()   
-    # plt.savefig(r'dir')
-    plt.show()
+    plt.savefig(r"\\engr-drive.bluecat.arizona.edu\Research\Ratcliff\Spencer Yeager\conferences\Fpi15-2023\assets\pbttt_seccm_fc_overlay.png", dpi='figure')
+    # plt.show()
 
 
 
