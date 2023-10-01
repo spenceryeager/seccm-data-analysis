9593e76d35c5f1a1e76ad1c8843149b7e4c54a9b
Spencer Yeager <yeager.spencer@gmail.com>
2023-10-01 13:48:08 -0700

    updated formatting and plotting

--- cv_overlay_stdev.py
+++ cv_overlay_stdev.py
@@ -21,7 +21,7 @@ def make_plot():
     fontsize = 40
     mpl.rcParams.update({'font.size': fontsize, 'figure.autolayout': True})
 
-    directory = r"R:\Spencer Yeager\data\NiOx_Project\2023\09_Sep\25Sep2023_PtButtonCellIodide\Fc_Mini-Scan"
+    directory = r"\\engr-drive.bluecat.arizona.edu\Research\Ratcliff\Spencer Yeager\data\NiOx_Project\2023\09_Sep\25Sep2023_PtButtonCellIodide\Fc_Mini-Scan"
     file_list = os.listdir(directory)
     v = 'Voltage (V)'
     i = 'Current (pA)'
@@ -38,30 +38,39 @@ def make_plot():
     fig, ax = plt.subplots(figsize=(14,10), tight_layout=True)
     cv_count = 0
     current_list = []
+
+
     for file in file_list:
+
+
         if file.endswith('.csv'):
             cv_count += 1
             data = pd.read_csv(os.path.join(directory, file), sep='\t') # loading in data
             second_sweep = int(len(data) / sweep_numbers) # getting length of data file to remove first sweep
             second_sweep = second_sweep * 2
+
+
             if cv_count == 1:
                 current_list.append(data[i][second_sweep:].to_numpy())
                 potentials = data[v][second_sweep:].to_numpy()
             else:
                 current_list.append((data[i][second_sweep:].to_numpy()))
-            # ax.plot(data[v][second_sweep:] * -1, data[i][second_sweep:], color='dodgerblue', alpha=0.05, linewidth=7)
+
+
     avg_current = np.average(current_list, axis=0)
     avg_current_len = len(avg_current)
     std_current = np.std(current_list, axis=0)
-    print(std_current)
     upper_bound = avg_current + std_current
     lower_bound = avg_current - std_current
-    print(avg_current)
-    print(upper_bound)
-    # print(potentials)
-    ax.plot(np.negative(potentials[:int(avg_current_len/2)]), avg_current[:int(avg_current_len/2)], color='red', linewidth=7)
-    # ax.fill_between(np.negative(potentials), y1 = (avg_current + std_current), y2 = (avg_current - std_current), color='red', linewidth=7, alpha=0.4, interpolate=True)
 
+
+    # sweep 1 plot
+    ax.plot(np.negative(potentials[:int(avg_current_len/2)]), avg_current[:int(avg_current_len/2)], color='red', linewidth=7)
+    ax.fill_between(np.negative(potentials[:int(avg_current_len/2)]), y1 = (avg_current[:int(avg_current_len/2)] + std_current[:int(avg_current_len/2)]), y2 = (avg_current[:int(avg_current_len/2)] - std_current[:int(avg_current_len/2)]), color='red', linewidth=0, alpha=0.4, interpolate=True)
+
+    # sweep 2 plot
+    ax.plot(np.negative(potentials[int(avg_current_len/2):]), avg_current[int(avg_current_len/2):], color='red', linewidth=7)
+    ax.fill_between(np.negative(potentials[int(avg_current_len/2):]), y1 = (avg_current[int(avg_current_len/2):] + std_current[int(avg_current_len/2):]), y2 = (avg_current[int(avg_current_len/2):] - std_current[int(avg_current_len/2):]), color='red', linewidth=0, alpha=0.4, interpolate=True)
 
     # ax.plot(np.negative(potentials), (avg_current - std_current), color='black')
 
@@ -79,13 +88,17 @@ def make_plot():
         ax.spines[axis].set_linewidth(3)
 
 
-    # if ev_axis:
-    #     ax2 = ax.secondary_xaxis("top", functions=(lambda x: (x-fc_ev)*-1, lambda x: (x+fc_ev)*-1))
-    #     ax2.minorticks_on()
-    #     ax2.set_xlabel("Energy vs. Vacuum (eV)")
+    if ev_axis:
+        ax2 = ax.secondary_xaxis("top", functions=(lambda x: (x-fc_ev)*-1, lambda x: (x+fc_ev)*-1))
+        ax2.minorticks_on()
+        ax2.set_xlabel("Energy vs. Vacuum (eV)")
+        ax2.tick_params(axis = 'both', direction='in', which='both', length=18, width=3)
+
+        for axis in ['top','bottom','left','right']:
+            ax2.spines[axis].set_linewidth(3)
 
 
-    # # plt.savefig(r'dir')
+    # plt.savefig(r"\\engr-drive.bluecat.arizona.edu\Research\Ratcliff\Papers in Progress 2023\Perovskites\Iodide-Redox-On-NiOx\Figures\ferrocene_calib.svg")
     plt.show()
 
 
