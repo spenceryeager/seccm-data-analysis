1b8bb3d94e17a9c16e7a1d44bd72ccfdbff90663
Spencer Yeager <yeager.spencer@gmail.com>
2023-10-02 14:12:12 -0700

    updated formatting etc etc.

--- figure-plotting/cv-overlay.py
+++ figure-plotting/cv-overlay.py
@@ -3,6 +3,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Arial']
+rcParams['font.weight'] = 'bold'
+rcParams['axes.labelweight'] = 'bold'
+
 
 
 def main():
@@ -10,44 +16,76 @@ def main():
 
 
 def make_plot():
-    # Setting font size before making plot
-    font = {'size': 12}
-    plt.rc('font', **font)
+    ##############################
+    # some parameters to change
+    directory = r'dir'
+    savefig_directory = r"savedir"
+    savefig_name = r"name"
+    sweep_numbers = 3
+    fc_calibration = 0.425
+    fc_ev = -4.9
+    reference = "Potential (V) vs Fc/Fc$^{+}$"
+    ev_axis = False
+    save = False
+    ##############################
 
-    directory = r"C:\Users\Spencer\Documents\data-analysis\28Mar2023_PBTTT_Fc\scan"
+    # Setting font size before making plot
+    fontsize = 40
+    mpl.rcParams.update({'font.size': fontsize, 'figure.autolayout': True})
+
     file_list = os.listdir(directory)
     v = 'Voltage (V)'
     i = 'Current (pA)'
     
-    # some parameters to change
-    sweep_numbers = 3
-    fc_calibration = 0
-    fc_ev = -4.9
+
 
     # creating plot
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(14,10), tight_layout=True)
     cv_count = 0
+
+
     for file in file_list:
+
+
         if file.endswith('.csv'):
             cv_count += 1
             data = pd.read_csv(os.path.join(directory, file), sep='\t') # loading in data
             second_sweep = int(len(data) / sweep_numbers) # getting length of data file to remove first sweep
-            ax.plot(data[v][second_sweep:] - fc_calibration, data[i][second_sweep:] * -1, color='dodgerblue', alpha=0.05)
+            ax.plot(data[v][second_sweep:] - fc_calibration, data[i][second_sweep:] * -1, color='red', alpha=0.05)
+
+
 
     # Formatting plot
     ax.invert_xaxis()
     ax.minorticks_on()
-    ax.set_xlabel("Potential (V) vs. Ag/AgCl")
+    ax.set_xlabel(reference)
     ax.set_ylabel("Current (pA)")
-    ax.set_ylim(-25,3)
     ax.legend(['n ='+str(cv_count)], handlelength=0, handletextpad=0)
 
-    # ax2 = ax.secondary_xaxis("top", functions=(lambda x: (x-fc_ev)*-1, lambda x: (x+fc_ev)*-1))
-    # ax2.minorticks_on()
-    # ax2.set_xlabel("Energy vs. Vacuum (eV)")
-    plt.tight_layout()   
-    plt.savefig(r"\\engr-drive.bluecat.arizona.edu\Research\Ratcliff\Spencer Yeager\conferences\Fpi15-2023\assets\pbttt_seccm_fc_overlay.png", dpi='figure')
-    # plt.show()
+    ax.xaxis.labelpad = 5
+    ax.yaxis.labelpad = 5
+    ax.tick_params(axis = 'both', direction='in', which='both', length=18, width=3)
+    
+
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(3)
+
+
+    if ev_axis:
+        ax2 = ax.secondary_xaxis("top", functions=(lambda x: (x-fc_ev)*-1, lambda x: (x+fc_ev)*-1))
+        ax2.minorticks_on()
+        ax2.set_xlabel("Energy vs. Vacuum (eV)")
+        ax2.tick_params(axis = 'both', direction='in', which='both', length=18, width=3)
+
+
+        for axis in ['top','bottom','left','right']:
+            ax2.spines[axis].set_linewidth(3)
+
+
+    if save:
+        plt.savefig(os.path.join(savefig_directory, savefig_name), dpi='figure')
+    
+    plt.show()
 
 
 
