52ab4637592770e5bdfe538eadfd1951328b5759
Spencer Yeager <yeager.spencer@gmail.com>
2026-02-10 11:10:02 -0700

    adding new file for handling outliers

---
+++ data-processing/handling_outliers.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.signal as signal
+import scipy.ndimage as ndimage
+import scipy.constants as constant
+import scipy.stats as stats
+
+def main():
+    print('hi')
+
+
+if __name__ == "__main__":
+    main()
