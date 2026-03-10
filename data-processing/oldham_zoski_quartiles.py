8d55fa78c59d6f6e4db950facea3e2d6daf06978
Spencer Yeager <yeager.spencer@gmail.com>
2026-03-09 17:00:53 -0700

    figuring this out

---
+++ data-processing/oldham_zoski_quartiles.py
@@ -0,0 +1,20 @@
+# making the contour plots shown in the Oldham and Zoski paper.
+# https://doi.org/10.1016/0022-0728(89)85029-6
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy as sp
+
+
+def main():
+    q = [0.25, 0.5, 0.75]
+    alpha = np.linspace(0, 1, 101)
+    knaught = np.linspace(0.2, 20, 201)
+
+    
+
+
+if __name__ == "__main__":
+    main()
+
