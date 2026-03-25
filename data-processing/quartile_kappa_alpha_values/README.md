552ebe1f8cf6b06682488e88c4625ad9ed410ea7
Spencer Yeager <yeager.spencer@gmail.com>
2026-03-25 12:47:21 -0400

    Quartile values are wrapped, I've crossreferenced them with the paper.

--- data-processing/quartile_kappa_alpha_values/README.md
+++ data-processing/quartile_kappa_alpha_values/README.md
@@ -1,3 +1,9 @@
 # Quartile KappaNaught and Alpha Values
 
 This is from the Oldham-Zoski paper on using quartiles to determine the kinetics of a sigmoid voltammogram. These three separate files are calculated from oldham_zoski_quartiles.py, and can be loaded & cross referenced to find a kinetics values. I'm leaving the option to increase the resolution of the calculations by changing the intervals of alpha & kappa naught. 
+
+BIG NOTE: When using these values, the transfer coefficient (symmetry of reaction) is displayed as is. KappaNaught, however, is NOT THE FINAL RATE COEFFICIENT VALUE!
+
+This is a dimensionless factor that combines different geometric factors & the standard rate coefficient. Its definition is...
+
+kappanaught = pi * k0 * a / (4 Do) where Do is the diffusion coeffiicent and a is the radius of the UME.
---
+++ data-processing/quartile_kappa_alpha_values/value_cleanup.py
@@ -0,0 +1,28 @@
+# this is just a small utility script to clean up the kinetics values
+
+import pandas as pd
+import os
+
+
+def main():
+    quartile_file = r"data-processing\quartile_kappa_alpha_values\quartile_vals_unabridged.csv" 
+    # path is relative to where I'm running it
+    save_file = r'data-processing\quartile_kappa_alpha_values'
+    savename = 'quartile_values_unabridged_cleaned'
+    quartile_values = pd.read_csv(quartile_file)
+    quartile_values = quartile_values.dropna()
+
+    quartile_values = quartile_values.loc[quartile_values['1/4 fsolve check (should be zero)'] < 1]
+    quartile_values = quartile_values.loc[quartile_values['1/2 fsolve check'] < 1]
+    quartile_values = quartile_values.loc[quartile_values['3/4 fsolve check'] < 1]
+    quartile_values = quartile_values.sort_values(by='E1/4 - E 3/4 (mV)')
+    
+    quartile_values.to_csv(path_or_buf=os.path.join(save_file, (savename + '.csv')), index=False)
+
+
+    
+
+
+
+if __name__ == '__main__':
+    main()
