ed511c5b1498bfa5f00243cdd9ba1c83ddafd715
Spencer Yeager <yeager.spencer@gmail.com>
2026-04-12 15:37:06 -0700

    Can iterate now

--- data-processing/oldham_zoski_batch_simulation.py
+++ data-processing/oldham_zoski_batch_simulation.py
@@ -10,15 +10,25 @@ def main():
     dr = do # we assume the diffusion of the redox probe is the same in the oxidized and reduced state
     n = 1 # number of electrons transfered
     a = 2.7 * (10 **-5) # radius of tip, in cm # This is NOT USED! 
-    eo = 0 # formal potential
+    eo = 0.5 # formal potential
     e = np.linspace(-1, 1, 10000) # potential range
     # e = e - eo
-    # kappanaught = 5
-    # transfer_coef = 0.5 # transfer coefficient
+    # kappanaught_list = [5]
+    # alpha_list = [0.5] # transfer coefficient
     kappanaught_list = np.linspace(2,20,2)
     alpha_list = np.linspace(0,1,5)
-    reduction = True # are we looking at a reduction process or an oxidation process?
-    # for 
+    reduction = False # are we looking at a reduction process or an oxidation process?
+    visualize_and_print = True
+
+    value_df = pd.DataFrame(columns=['dE1/4 - dE3/4 (mV)', "dEo - dE1/2 (mV)", "Transfer Coefficient", "KappaNaught"])
+    delta_quartile = []
+    delta_half =[]
+    alpha = []
+    kn_list = []
+
+
+
+
     for kappanaught in kappanaught_list:
 
         for transfer_coef in alpha_list:
@@ -41,14 +51,44 @@ def main():
                 one_half = voltammogram.loc[(voltammogram['Normalized Current'] - -0.5).abs().argsort()[0]]
                 three_quart = voltammogram.loc[(voltammogram['Normalized Current'] - -0.75).abs().argsort()[0]]
 
-            print((one_half['Potential (V)'] * 1000))
-            visualize(voltammogram, one_quart, one_half, three_quart, reduction)
+            
+            # List Appending
+            if (np.abs(one_quart['Potential (V)'] - three_quart['Potential (V)']) * 1000) < 400:
+                delta_quartile.append(np.abs(one_quart['Potential (V)'] - three_quart['Potential (V)']) * 1000)
+                delta_half.append(np.abs(np.abs(eo - one_half['Potential (V)']) * 1000))
+                alpha.append(transfer_coef)
+                kn_list.append(kappanaught)
+            
+            else:
+                delta_quartile.append(np.nan)
+                delta_half.append(np.nan)
+                alpha.append(np.nan)
+                kn_list.append(np.nan)
+
+                
+            # Visualization and Printing Values
+            if visualize_and_print:
+                print("########")
+                print("E1/2 (mV)", np.abs(np.abs(eo - one_half['Potential (V)']) * 1000))
+                print("E1/4 - E3/4 (mV)",np.abs(one_quart['Potential (V)'] - three_quart['Potential (V)']) * 1000)
+                print("Transfer Coefficient:", transfer_coef)
+                print("KappaNaught:", kappanaught)
+                visualize(voltammogram, one_quart, one_half, three_quart, reduction)
+
+
+    # Dataframe Saving
+    value_df['dE1/4 - dE3/4 (mV)'] = delta_quartile
+    value_df['dEo - dE1/2 (mV)'] = delta_half
+    value_df['Transfer Coefficient'] = alpha
+    value_df['KappaNaught'] = kn_list
+
+    print(value_df.head())
 
 
 def visualize(voltammogram, one_quart, one_half, three_quart, reduction):
     fig, ax = plt.subplots()
     ax.plot(voltammogram['Potential (V)'], voltammogram['Normalized Current'])
-    ax.set_xlim(-0.6, 0.6)
+    ax.set_xlim(-0.2, 1)
     ax.invert_xaxis()
     ax.set_xlabel("Potential (V)")
     ax.set_ylabel('Normalized Current')
