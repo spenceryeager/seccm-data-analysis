b84e7df729638a051386d30516209baba19e4a1e
Spencer Yeager <yeager.spencer@gmail.com>
2026-06-17 13:16:06 -0700

    updating the plotting code

--- figure-plotting/cv-single.py
+++ figure-plotting/cv-single.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 import scipy.signal as signal
+import scipy.ndimage as ndimage
 import matplotlib as mpl
 from matplotlib import rcParams
 import os
@@ -22,52 +23,56 @@ def main():
 def fit_data():
     # set figure font sizes
     plt.rcParams['font.size'] = 20
-    # fit data?
-    fit = False
+    # smooth data?
+    fit = True
+    # IUPAC format?
+    iupac = False
     reference_label = "Potential (V) vs. Ag Wire"
 
-    data_file = r"G:\RDrive_Backup\Spencer Yeager\papers\paper4_pbtttt_p3ht_transfer_kinetics\data\28Mar2023_PBTTT_Fc\scan\0X_0Y_pbttt_fc.csv"
-    save_dir = r"R:\Spencer Yeager\data\NiOx_Project\2023\09_Sep\20Sep2023_ButtonPtCel\figures"
+    data_file = r"G:\RDrive_Backup\Spencer Yeager\papers\paper4_pbtttt_p3ht_transfer_kinetics\data\SECCM_Data\02June2026_rrP3HT_Colocation\reference_calibration\reference_before_5.csv"
     save_name = "fc_approach.svg"
     data_read = pd.read_csv(data_file, sep='\t')
     
-
-    # removing the first approach scan by dividing the length by # of sweeps and starting after the first sweep
-    sweeps = 2
-    sweep_intervals = int(len(data_read) / sweeps)
-    data_subset = data_read[sweep_intervals:sweep_intervals*2]
     
-    fontsize = 40
+    if not iupac:
+        data_read['Current (pA)'] = np.negative(data_read['Current (pA)'].copy())
+
+        
+
+    fontsize = 25
     mpl.rcParams.update({'font.size': fontsize, 'figure.autolayout': True})
-    fig, ax = plt.subplots(figsize=(14,10), tight_layout=True)
+    fig, ax = plt.subplots(figsize=(8,8), tight_layout=True)
     if fit == True:
-        ax.plot(data_subset["Voltage (V)"], data_subset["Current (pA)"] * -1, color='black', alpha=0.2, label="Raw Current")
-        fitted_current  = current_fit(data_subset['Current (pA)'])
-        ax.plot(data_subset["Voltage (V)"], fitted_current * -1, color='black', alpha=1, label="Savitzky-Golay Current")
+        ax.plot(data_read["Voltage (V)"], data_read["Current (pA)"], color='black', alpha=0.2, label="Raw Current", linewidth=7)
+        fitted_current  = current_fit(data_read['Current (pA)'])
+        ax.plot(data_read["Voltage (V)"], fitted_current, color='black', alpha=1, label="Smoothed Current", linewidth=7)
         ax.legend(prop={'size': 16})
     else:
-        ax.plot(data_subset["Voltage (V)"], data_subset["Current (pA)"] * -1, color='black', alpha=1, linewidth=7)
+        ax.plot(data_read["Voltage (V)"], data_read["Current (pA)"], color='black', alpha=1, linewidth=7)
 
     ax.invert_xaxis()
     ax.set_ylabel("Current (pA)")
     ax.set_xlabel(reference_label)
     ax.xaxis.labelpad = 5
     ax.yaxis.labelpad = 5
-    ax.tick_params(axis = 'both', direction='in', which='both', length=18, width=3)
-    
+    ax.minorticks_on()
+    ax.tick_params(axis = 'both', direction='in', which='major', length=9, width=3)
+    ax.tick_params(axis = 'both', direction='in', which='minor', length=4.5, width=3)
+
     
     for axis in ['top','bottom','left','right']:
         ax.spines[axis].set_linewidth(3)
     
     # plt.savefig(os.path.join(save_dir, save_name))
+    ax.set_box_aspect
     plt.show()
 
 
 def current_fit(current):
     # applying a savitzky golay filter
     savgol_current = signal.savgol_filter(current, 50, 5)
-    print(savgol_current)
-    return savgol_current
+    cleaned_current = ndimage.gaussian_filter1d(savgol_current, 15)
+    return cleaned_current
 
 
 if __name__ == "__main__":
